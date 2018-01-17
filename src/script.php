6460b95c0cc636b0264c0c76b8c0c892420d9de2
Joe Hall <joefhall@gmail.com>
2018-01-17 16:03:53 +0000

    Update script

--- src/script.php
+++ src/script.php
@@ -5,5 +5,4 @@ require_once('EngagingNetworksDataExport/DataExport.php');
 use EngagingNetworksDataExport\DataExport;
 
 $dataExport = new DataExport();
-
-print_r($dataExport->handle());
+$dataExport->handle();
